5a9fa9a3f9d9e9a807b80a27cddd8ae53cc45b6a
Dan Hall <dan@qed-productions.com>
2023-09-15 16:07:33 +0400

    tidy up before new branch and flat file

--- src/variables.js
+++ src/variables.js
@@ -1,18 +1,5 @@
 module.exports = async function (self) {
 	var variable_array = []
-	// var variable_array = [
-	// 	{ variableId: 'input_count', name: 'Input Count' },
-	// 	{ variableId: 'output_count', name: 'Output Count' },
-	// ]
-
+	
 	self.setVariableDefinitions(variable_array)
 }
-
-// module.exports = function getOutputNames(outputs) {
-
-// 	for (let i = 0; i < Math.ceil(outputs/32); i++) {
-// 		this.log('debug',`outputs  = ${outputs}`)
-// 		this.log('debug',`i  = ${i}`)
-// 	}
-
-// }
